732e9a91761a82fa53d073cf90d2fafa73329444
Ambush-Developer <1797249658@qq.com>
2026-04-27 15:37:10 +0800

    后端创建

---
+++ backend/Flight-Information-Tracker/src/main/java/com/example/flightinformationtracker/FlightInformationTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.flightinformationtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightInformationTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightInformationTrackerApplication.class, args);
+    }
+
+}
---
+++ backend/Flight-Information-Tracker/src/test/java/com/example/flightinformationtracker/FlightInformationTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flightinformationtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightInformationTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
